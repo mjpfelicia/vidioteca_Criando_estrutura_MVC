5b8f4435c4176cb0c9f219f815f52b8589a6833f
Sandra Felicio <mjpfelicia@gmail.com>
2024-03-17 09:45:31 -0300

    Criando o controller

---
+++ index.js
@@ -0,0 +1,18 @@
+//BIBLIOTECAS/MODULOS UTILIZADOS
+
+const express = require("express");
+const app = express();
+const database = require("./db/db");
+const Video = require("./models/Video");
+
+
+
+//SINCRONISMO COM O BANCO DE DADOS
+try {
+  database.sync().then(() => {});
+  app.listen(9443, () => {
+    console.log("Servidor rodando");
+  });
+} catch (erro) {
+  console.log("Houve uma falha ao sincronizar com o banco de dados. ", erro);
+}
